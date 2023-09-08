e0e6176dcc46204339b504bee6aa8413a2936815
anissalopez <anissa965@gmail.com>
2023-09-08 15:49:50 -0400

    working with lists

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    badge = f"Hello, my name is {name}."
+    return badge
 
 def batch_badge_creator(names):
-    return None
+    badges = [(f"Hello, my name is {badge}.") for badge in names]
+    return badges
 
 def assign_rooms(names):
-    return None
+    room_assignments = list()
+    i= 0
+    while i < 8:
+        message = f"Hello, {names[i]}! You'll be assigned to room {i + 1}!"
+        room_assignments.append(message)
+        i += 1
+    return room_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    assignments = assign_rooms(names)
+    for rooms in assignments:
+        print(rooms)
+
+
